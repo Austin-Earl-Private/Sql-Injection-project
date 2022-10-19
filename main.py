507c04ac7c1719d0d49f4e74350f00222a2bdeec
Austin-Earl-Private <the.qmind2020@gmail.com>
2022-10-18 20:05:01 -0600

    Created basic Framework

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import re
 
-# f"select * from test where username= '{}' and password = '{}'"
-test_strings = ["""select * from test where username="test" and password = "hacked" """]
-comment = "' OR 1=1 and ''='\\"
+valid = [("test","test")]
+tautology = [("test","' or 1=1 --")]
+union = [("test","' union select * from secrets --")]
+add_stm = [("test","'; delete * from users where ''='")]
+comment = [("test ' --","")]
 
+def generate_query(username,password):
+    return f"select * from users where username='{username}' and password='{password}';"
 
-
+def test_query(username,password,sanitizer):
+    print(f"\tQuery before sanitization:  {generate_query(username,password)}")
+    print(f"\tQuery after sanitization:  {generate_query(sanitizer(username),sanitizer(password))}")
+    print("")
 
 def sanitize(value):
-    # No " union " no " or " no "'" no "--" no ";"
-    result= re.sub(r"\bunion\b|\bor\b|'|--|;|\\","",value,flags=re.IGNORECASE,count=0)
-    # print(result)
-    print(result)
-    return result
+    return value
 
 if __name__ == "__main__":
-    print(f"select * from test where username='{sanitize(comment)}' and password = '{sanitize('test;')}';")
-    # print()
+    print("Testing Valid Queries:")
+    for set in valid:
+        test_query(set[0],set[1],sanitize)
+    print("Testing Tautology Queries:")
+    for set in tautology:
+        test_query(set[0],set[1],sanitize)
+    print("Testing Union Queries:")
+    for set in union:
+        test_query(set[0],set[1],sanitize)
+    print("Testing Added Statement Queries:")
+    for set in add_stm:
+        test_query(set[0],set[1],sanitize)
+    print("Testing Comment Queries:")
+    for set in comment:
+        test_query(set[0],set[1],sanitize)
