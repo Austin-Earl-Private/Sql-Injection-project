9efcab5040915fed74334c15d186a16661159484
Austin-Earl-Private <the.qmind2020@gmail.com>
2022-10-18 20:23:17 -0600

    comments and clean up

--- main.py
+++ main.py
@@ -6,17 +6,18 @@ union = [("test", "' union select * from secrets --")]
 add_stm = [("test", "'; delete * from users where ''='")]
 comment = [("test ' --", "")]
 
-
-def generate_query(username, password):
+# all generate queries must accept two values.
+def generate_user_query(username, password):
     return f"select * from users where username='{username}' and password='{password}';"
 
 
-def test_query(username, password, sanitizer):
-    print(f"\tQuery before sanitization:  {generate_query(username, password)}")
-    print(f"\tQuery after sanitization:  {generate_query(sanitizer(username), sanitizer(password))}")
+# value1 and value 2 will go into the query. Sanitizer only sanitizes one element at a time
+def test_query(value1, value2, sanitizer, query):
+    print(f"\tQuery before sanitization:  {query(value1, value2)}")
+    print(f"\tQuery after sanitization:  {query(sanitizer(value1), sanitizer(value2))}")
     print("")
 
-
+# input has bad elements removed and then after being cleaned is returned
 def sanitize(value):
     return value
 
@@ -24,19 +25,19 @@ def sanitize(value):
 def automated_tests():
     print("Testing Valid Queries:")
     for set in valid:
-        test_query(set[0], set[1], sanitize)
+        test_query(set[0], set[1], sanitize,generate_user_query)
     print("Testing Tautology Queries:")
     for set in tautology:
-        test_query(set[0], set[1], sanitize)
+        test_query(set[0], set[1], sanitize,generate_user_query)
     print("Testing Union Queries:")
     for set in union:
-        test_query(set[0], set[1], sanitize)
+        test_query(set[0], set[1], sanitize,generate_user_query)
     print("Testing Added Statement Queries:")
     for set in add_stm:
-        test_query(set[0], set[1], sanitize)
+        test_query(set[0], set[1], sanitize,generate_user_query)
     print("Testing Comment Queries:")
     for set in comment:
-        test_query(set[0], set[1], sanitize)
+        test_query(set[0], set[1], sanitize,generate_user_query)
 
 
 def manual_test():
@@ -54,6 +55,7 @@ def menu():
         if test_type == 1 or test_type == 2:
             return test_type
         else:
+            print("Invalid input\n")
             continue
 
 
