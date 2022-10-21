18be21b9c9f609db9c7bb95dd0b73e7fa9593a1f
Hicken-Fredrick <Miroku101@gmail.com>
2022-10-20 21:08:27 -0600

    last push

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ def genQueryStrong(uName, password):
    print("BASE QUERY")
    print(f"SELECT * FROM users WHERE username='{uName}' and password='{password}';")
    print("STRONG MITIGATION QUERY")
-   #reducing strings to lower and sanitizing the string
-   str.lower(uName)
+   #reducing strings to lower and sanitizing the string for added security
+   sanitizedUName = str.lower(sanitizeString(uName))
    #by changing the way the query is setup and only getting the information needed 
    #closes a lot of holes as well as allowing passowrds to be more complex
-   print(f"SELECT password FROM users WHERE username='{sanitizeString(uName)}';\n")
+   print(f"SELECT password FROM users WHERE username='{sanitizedUName}';\n")
 
 #sanitize string following rule "username and the password consist of letters, numbers, and underscores"
 def sanitizeString(string):
@@ -32,10 +32,10 @@ def sanitizeString(string):
 
 #run valid test cases  !!! need everyone in team to create 1 set for each test set per instructions !!!
 def testValid(queryGen):
-   validTests = [["]Austin", "pass123"],
-   ["FrEd", "SuperSecure"],
-   ["Brad", "CrayCray"],
-   ["Daniel", "passpass321"],
+   validTests = [["Austin", "pass123"],
+   ["Fr_Ed", "SuperSecure"],
+   ["_Brad", "CrayCray"],
+   ["Daniel_", "passpass321"],
    ["Spencer", "123456789"]]
    print("----  VALID TESTS    ----\n")
    for validTest in validTests:
@@ -44,11 +44,11 @@ def testValid(queryGen):
 
 #run tautology test cases
 def testTautology(queryGen):
-   tautologyTests = [["test", "' or 1=1 --"],
-   ["test", "' or 1=1 --"],
-   ["test", "' or 1=1 --"],
-   ["test", "' or 1=1 --"],
-   ["test", "' or 1=1 --"]]
+   tautologyTests = [["Austin", "' or 1=1 --"],
+   ["Fr_Ed", "' or 1=1 --"],
+   ["_Brad", "' or 1=1 --"],
+   ["Daniel_", "' or 1=1 --"],
+   ["Spencer", "' or 1=1 --"]]
    print("----  TAUTOLOGY TESTS    ----\n")
    for tautologyTest in tautologyTests:
       queryGen(tautologyTest[0], tautologyTest[1])
@@ -56,11 +56,11 @@ def testTautology(queryGen):
 
 #run union test cases
 def testUnion(queryGen):
-   unionTests = [["test", "' UNION SELECT * from secrets --"],
-   ["test", "' union select * from secrets --"],
-   ["test", "' union select * from secrets --"],
-   ["test", "' union select * from secrets --"],
-   ["test", "' union select * from secrets --"]]
+   unionTests = [["Austin", "' UNION SELECT * from secrets --"],
+   ["Fr_Ed", "' union select * from secrets --"],
+   ["_Brad", "' union select * from secrets --"],
+   ["Daniel_", "' union select * from secrets --"],
+   ["Spencer", "' union select * from secrets --"]]
    print("----  UNION TESTS    ----\n")
    for unionTest in unionTests:
       queryGen(unionTest[0], unionTest[1])
@@ -68,11 +68,11 @@ def testUnion(queryGen):
 
 #run additional statement test cases
 def testAddState(queryGen):
-   addStatementTests = [["test", "'; delete * from users where ''='"],
-   ["test", "'; delete * from users where ''='"],
-   ["test", "'; delete * from users where ''='"],
-   ["test", "'; delete * from users where ''='"],
-   ["test", "'; delete * from users where ''='"]]
+   addStatementTests = [["Austin", "'; delete * from users where ''='"],
+   ["Fr_Ed", "'; delete * from users where ''='"],
+   ["_Brad", "'; delete * from users where ''='"],
+   ["Daniel_", "'; delete * from users where ''='"],
+   ["Spencer", "'; delete * from users where ''='"]]
    print("----  ADD STATEMENT TESTS    ----\n")
    for addStatmentTest in addStatementTests:
       queryGen(addStatmentTest[0], addStatmentTest[1])
@@ -80,11 +80,11 @@ def testAddState(queryGen):
 
 #run comment test cases
 def testComment(queryGen):
-   commentTests = [["test ' --", ""],
-   ["test ' --", ""],
-   ["test ' --", ""],
-   ["test ' --", ""],
-   ["test ' --", ""]]
+   commentTests = [["Austin ' --", ""],
+   ["Fr_Ed ' --", ""],
+   ["_Brad ' --", ""],
+   ["Daniel_ ' --", ""],
+   ["Spencer ' --", ""]]
    print("----  COMMENT TESTS    ----\n")
    for commentTest in commentTests:
       queryGen(commentTest[0], commentTest[1])
